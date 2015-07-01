e33681244e610233ab142689246e8b36094acc99
Bian Shifeng <bianshifeng@live.com>
2015-07-01 09:57:35 +0000

    del

--- mongoose.js
+++
@@ -1,7 +0,0 @@
-var mongoURI = "mongodb://localhost:27017/blog";
-var mongoose = require("mongoose");
-var MongoDB = mongoose.connect(mongoURI).connection;
-MongoDB.on('error', function(err) { console.log(err); });
-MongoDB.once('open', function() {
-  console.log("mongodb connection open");
-});
